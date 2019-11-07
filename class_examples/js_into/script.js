34f328b007cb0ecd6762c6c3afaf60f8130d37bb
Megan Page <mvp76@miami.edu>
2019-11-06 19:43:10 -0500

    jQuery assignment
    Spilling my pesto recipe!

---
+++ class_examples/js_into/script.js
@@ -0,0 +1,15 @@
+//var array1=[1, 2, 3, 5, 6];
+//var array2= ["This is a string", 'This is also a string'];
+//var heading = document.getElementByTag("h1");
+//consol.log(heading); /* console.log prints whatever is inside of it */
+//console.log(document.getElementsByTagName("h1"));//
+
+$(document).ready(function () {
+
+//all your javascript goes here
+  var x = 2+2;
+  console.log(x);
+  console.log("Everything loaded!");
+  console.log("this function ran");
+
+});
